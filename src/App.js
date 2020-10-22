8fd11fc521677206c0c28c6fbf5f4a681ec869ed
Scott Katzelnick <scott@devsmk.com>
2020-10-21 23:24:17 -0400

    Created custom dropdown react-hook and updated .eslintrc.json to incorporate new react-hook rules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Ellie" animal="Dog" breed="Golden Retreiver" />
-      <Pet name="LT" animal="Cat" breed="Black Cat" />
-      <Pet name="Oliver" animal="Dog" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
